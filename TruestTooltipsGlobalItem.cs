f1e2b06b0b071b86daf700dab296fac6f5a881a1
Stevie <xxlennygamerxx@gmail.com>
2020-07-28 14:39:22 -0700

    Create TruestTooltipsGlobalItem.cs

---
+++ TruestTooltipsGlobalItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace TruestTooltips
+{
+    public class TruestTooltipsGlobalItem : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            base.ModifyTooltips(item, tooltips);
+        }
+    }
+}
